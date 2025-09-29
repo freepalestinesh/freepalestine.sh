c52d595880d28c20eef8e9a6cae505f67878c7f8
free palestine <salad1nn@protonmail.com>
2025-09-30 00:49:53 +0200

    Update Index.tsx with new content

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1,1 +1,9 @@
-<h1>An independent, international, text-based blog. Minimal, fast, accessible.</h1>
+import React from 'react';
+
+export default function Index() {
+  return (
+    <p className="mt-2 text-muted-foreground">
+      An independent, international, text-based blog. Minimal, fast, accessible.
+    </p>
+  );
+}
