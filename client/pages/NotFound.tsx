8f6d17d648e60fa7d6a35401e3dea6bdbc678e08
free palestine <salad1nn@protonmail.com>
2025-09-29 23:37:51 +0200

    Update NotFound.tsx

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -15,9 +15,9 @@ const NotFound = () => {
     <div className="min-h-[50vh] flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-2">404</h1>
-        <p className="text-base text-muted-foreground mb-4">Seite nicht gefunden</p>
+        <p className="text-base text-muted-foreground mb-4">Page not found</p>
         <Link to="/" className="underline underline-offset-4">
-          Zurück zur Startseite
+          back to home
         </Link>
       </div>
     </div>
