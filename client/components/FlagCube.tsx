4353de1b8625f667507cadb4515b25fbf08c3d6b
free palestine <salad1nn@protonmail.com>
2025-10-01 20:10:39 +0200

    Update FlagCube.tsx

--- client/components/FlagCube.tsx
+++ client/components/FlagCube.tsx
@@ -1,47 +1,72 @@
 import React from "react";
 import "./flag-cube.css";
 
-type FlagCubeProps = {
-  scale?: number;
-};
-
-export default function FlagCube({ scale = 1 }: FlagCubeProps) {
+/**
+ * Original-Struktur wiederhergestellt:
+ * container -> 3 * cube (Depth-Layer) -> je 3 Spalten -> je 3 span (Höhe)
+ * CSS berechnet Offsets aus --layer (auto), --x, --i
+ */
+export default function FlagCube() {
   return (
-    <div className="flag-cube-root" style={{ ["--fc-scale" as any]: scale }}>
+    <div className="flag-cube-root">
       <div className="flag-cube-scale" data-cube>
-        <div className="cube-grid">
-          {/* 3 Layer à 3 Säulen à 3 Elemente */}
-          {[0, 1, 2].map(layer => (
-            <div className="cube-row" key={layer} data-layer={layer}>
-              {[-1, 0, 1].map(x => (
-                <div
-                  className="cube-stack"
-                  key={x}
-                  style={{ ["--x" as any]: x, ["--y" as any]: 0 }}
-                >
-                  {[3, 2, 1].map(i => {
-                    // Beispiel-Farbzuordnung (anpassen!):
-                    // Obere Reihe farbig, Rest neutral
-                    let cls = "";
-                    if (layer === 0) {
-                      if (x === -1) cls = "c-red";
-                      if (x === 0) cls = "c-black";
-                      if (x === 1) cls = "c-green";
-                    } else if (layer === 1) {
-                      cls = "c-white";
-                    }
-                    return (
-                      <span
-                        key={i}
-                        style={{ ["--i" as any]: i }}
-                        className={cls}
-                      />
-                    );
-                  })}
-                </div>
-              ))}
+        <div className="container">
+          {/* Layer 0 */}
+          <div className="cube" data-layer="0">
+            <div style={{ ["--x" as any]: -1, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
             </div>
-          ))}
+            <div style={{ ["--x" as any]: 0, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
+            </div>
+            <div style={{ ["--x" as any]: 1, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
+            </div>
+          </div>
+
+          {/* Layer 1 */}
+          <div className="cube" data-layer="1">
+            <div style={{ ["--x" as any]: -1, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
+            </div>
+            <div style={{ ["--x" as any]: 0, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
+            </div>
+            <div style={{ ["--x" as any]: 1, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
+            </div>
+          </div>
+
+            {/* Layer 2 */}
+          <div className="cube" data-layer="2">
+            <div style={{ ["--x" as any]: -1, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
+            </div>
+            <div style={{ ["--x" as any]: 0, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
+            </div>
+            <div style={{ ["--x" as any]: 1, ["--y" as any]: 0 }}>
+              <span style={{ ["--i" as any]: 3 }} />
+              <span style={{ ["--i" as any]: 2 }} />
+              <span style={{ ["--i" as any]: 1 }} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
