b43801a78a683b1b34306fe27ec1fbc13df8a803
freepalestinesh <freepalestinesh@users.noreply.github.com>
2025-10-01 12:39:35 +0000

    deploy: 623240c1279b97274f3e3e18ddc5c3a42242e1d0

---
+++ assets/goals-XBhHkXcm.js
@@ -0,0 +1,50 @@
+const e=`---
+title: "Objectifs"
+date: "2025-09-29"
+tags: ["objectifs"]
+excerpt: "Déclaration de base : indépendance, position anti-sioniste, transparence, structure planifiée et participation communautaire."
+---
+À ma compréhension, il s’agit d’un blog libre et indépendant tenu par une personne anonyme et indépendante, engagée pour la justice. La position est un anti-sionisme à 100 % et une critique à 100 % du gouvernement israélien (de droite) de Netanyahou (Bibi). J’exprime mon plein soutien à la justice.
+
+Quelles que soient les tentatives de certaines personnes ou États de discréditer ou nuire à ce blog, il restera accessible en ligne. Soyez assurés qu’il demeurera disponible. Je sais que cela peut être difficile, mais la vérité finira par émerger.
+
+Vous vous demandez peut-être la nature et la finalité de ce blog.
+À mon avis personnel, le domaine freepalestine.sh est très pertinent. Le blog vise l’éducation et la sensibilisation aux risques d’escalade, à leurs causes et aux processus impliqués.
+
+J’ai eu le privilège d’étudier la situation au Moyen-Orient durant des années, inspiré par des figures telles que Peter Scholl-Latour et Michael Lüders.
+
+Elles ont nourri mon inspiration et motivé mes efforts de publication.
+
+Je souhaite toutefois présenter ma propre perspective. En ligne. De façon transparente.
+
+Que signifie « transparent » ?
+Mes recherches et sources sont disponibles sur le site. Je m’efforcerai d’exposer clairement afin d’éviter toute accusation de mensonge ou dissimulation. Ce sont des sources généralement reconnues.
+
+Je suis très curieux de la suite !
+J’espère commencer bientôt. Trame proposée :
+
+Si possible : court billet sur …
+
+Si possible : genèse du conflit et origines (clé pour comprendre le passé).
+
+Si possible : point sur la situation actuelle.
+
+Si possible : partager mes « prévisions » et évaluations.
+
+Ensuite, transformer en vidéos sur :
+
+YouTube  
+TikTok  
+Instagram  
+Twitch  
+Snapchat  
+
+Code du site vérifié quotidiennement via l’Internet Archive et ré‑upload si nécessaire.
+
+Après les billets, consultation de la communauté pour le contenu suivant (sujet avec le plus haut pourcentage de votes).
+
+Je ne suis PAS antisémite.  
+Je suis POUR la vie juive. Je suis POUR la vie palestinienne.
+
+Je ne soutiens PAS les régimes sionistes abusant de la population et des otages comme prétexte. L’Histoire est en train d’être réécrite. Netanyahou figurera parmi les dirigeants perçus comme des dictateurs du XXIᵉ siècle.
+`;export{e as default};
